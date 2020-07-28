b0ded0be072e618f6cc5d84757e3f6e3dcb35663
ryjonespdx <ryjones@pdx.edu>
2020-07-27 23:23:01 -0700

    Implemented toggle display button for skill tree

--- src/app/skill-tree/skill-tree.component.ts
+++ src/app/skill-tree/skill-tree.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-skill-tree',
   templateUrl: './skill-tree.component.html',
-  styleUrls: ['./skill-tree.component.sass']
+  styleUrls: ['./skill-tree.component.sass'],
 })
 export class SkillTreeComponent implements OnInit {
+  public show: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  // From: https://www.encodedna.com/angular/how-to-show-hide-or-toggle-elements-in-angular-4.htm
+  toggleDisplay() {
+    this.show = !this.show;
   }
-
 }
