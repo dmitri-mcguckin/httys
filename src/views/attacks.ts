9b973e6b2673bf81ed9a6d6e628fe3b169768447
movshov <movshov@pdx.edu>
2020-08-13 01:25:11 -0700

    Working version of attacks finished. Had to hardcode Total value for percentages with randomizer

--- src/views/attacks.ts
+++ src/views/attacks.ts
@@ -1,40 +1,75 @@
+import { thistle } from 'color-name';
+import { currencies } from './bits-currency';
+import { SystemJsNgModuleLoader } from '@angular/core';
+import { debuglog } from 'util';
+
 export interface attacks {
     start_time: number;
-    elapsed: number;
+    total: number;
+    completed: number;
+    attack_name: string;
     
-    check_time(start_time: number, elapsed:number): boolean;
+    check_time(currency:currencies, attacking: attacks): number;
+    start();
+    reward();
+    choose_attack(currency: currencies, attacking:attacks);
 }
 
 export class attack implements attacks{
-    start_time = null;
-    elapsed = null;
+    start_time = 0;
+    total = 0;
+    attack_name = null;
+    completed = 0;
 
-    constructor(){
-        start_time: null;
-        elapsed: 0;
+
+    constructor(start_time: number, elapsed: number, remaining: number, attack_name: string){
+        this.start_time = 0;
+        this.total = 0;
+        this.completed = 0;
+        this.attack_name = null;
+  
     }
 
-    /**Check if contract is up. If so return true othewise return false */
-    check_time(start_time: number, elapsed: number): boolean{
-        var current_time = new Date().getTime();
-        let duration = (current_time - start_time) * 1000;   //gives us time in milliseconds. 
+    /**Check if contract is up. Return duration so we can set percetage bar in main page.*/
+    check_time(currency: currencies, attacking: attacks): number{
+        let current_time = new Date().getTime();
+        attacking.completed = (current_time - attacking.start_time) * 1000;   //gives us time in milliseconds. 
 
-        if(duration >= elapsed){
-            return true;
+        if(attacking.completed >= attacking.total){   //we are done attacking. reset everything.
+            currency.bits += this.reward();
+            attacking.attack_name = null;
+            attacking.completed = 0;
+            return;
         }
         else{
-            return false;
+            return attacking.completed;   //return the remaining amount of time.
         }
     }
 
-    start(start_time: number){
-        start_time = new Date().getTime();
-        return start_time;
+    start(){
+        this.start_time = new Date().getTime();
+        return this.start_time;
     }
 
-    set_duration(elapsed: number){
-        elapsed = Math.floor(Math.random() * 80) + 1; //generate a random number between 1 and 60
+    reward(){
+        let reward = Math.floor(Math.random() * 500) + 1; //genearte a random number between 1 and 100.
+        return reward;
     }
-
+    
+    choose_attack(currency: currencies, attacking:attacks){
+        if (attacking.attack_name == null){   //we don't have an attack selected. 
+            var choice = (document.getElementById("malwares")) as HTMLSelectElement;
+            var total = choice.options[choice.selectedIndex].value;
+            attacking.attack_name = choice.outerHTML;
+            //attacking.elapsed = parseInt(total, 10);
+            attacking.total = Math.floor(Math.random() * (10000000000 - 1000000)+ 1000000);  //Couldn't read from HTML so doing this instead. 
+            attacking.start_time = this.start();  //get start_time from function above. 
+            return attacking.attack_name;
+        }
+        else{   //we are still working on an attack. 
+            return attacking.attack_name;
+        }
+    
+    }
 
 }
