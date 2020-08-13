d2f6de83436ab64a562e297073745fe2d3e7d7c0
ryjonespdx <ryjones@pdx.edu>
2020-08-13 04:55:55 -0700

    Implemented purchasing skills: setting purchased to true, deducting skill cost from user's skill points, updating display of avail skill points, and filling in skill's box in skill tree

--- src/app/skill-tree/skill-tree.component.ts
+++ src/app/skill-tree/skill-tree.component.ts
@@ -1,13 +1,12 @@
 // ng-bootstrap modal from https://ng-bootstrap.github.io/#/components/modal/examples
-import { Component, OnInit, Directive, ViewChild } from '@angular/core';
-import { BuiltinType } from '@angular/compiler';
-import { $ } from 'protractor';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 interface Skill {
   name: string;
   desc: string;
   cost: number;
+  purchased: boolean;
 }
 
 @Component({
@@ -16,14 +15,46 @@ interface Skill {
   styleUrls: ['./skill-tree.component.sass'],
 })
 export class SkillTreeComponent implements OnInit {
-  skillPoints: number = 0;
+  skillPoints: number = 2;
+  skillId: string;
   canPurchase: boolean = false;
   showTree: boolean = false;
-  skillInfo: Record<string, Skill> = {
+  allSkills: Record<string, Skill> = {
     'infection-1': {
       name: 'Bit Boost',
       desc: '+25% passive bit gain',
       cost: 1,
+      purchased: false,
+    },
+    'infection-2': {
+      name: 'TODO',
+      desc: 'TODO',
+      cost: 1,
+      purchased: false,
+    },
+    'infection-3': {
+      name: 'TODO',
+      desc: 'TODO',
+      cost: 1,
+      purchased: false,
+    },
+    'infection-4': {
+      name: 'TODO',
+      desc: 'TODO',
+      cost: 1,
+      purchased: false,
+    },
+    'infection-5': {
+      name: 'TODO',
+      desc: 'TODO',
+      cost: 1,
+      purchased: false,
+    },
+    'infection-6': {
+      name: 'TODO',
+      desc: 'TODO',
+      cost: 1,
+      purchased: false,
     },
   };
 
@@ -45,29 +76,51 @@ export class SkillTreeComponent implements OnInit {
   }
 
   purchaseSkill() {
-    alert('purchased');
+    this.allSkills[this.skillId].purchased = true;
+    this.skillPoints -= this.allSkills[this.skillId].cost;
+    document.getElementById('footer-skill-points').innerText =
+      'Skill points available: ' + this.skillPoints.toString();
+    document.getElementById(
+      'skill-points'
+    ).innerText = this.skillPoints.toString();
+    let currSkill = document.getElementById(this.skillId);
+    if (this.skillId.search('infect') > -1) {
+      currSkill.style.backgroundColor = 'maroon';
+    } else if (this.skillId.search('profit') > -1) {
+      currSkill.style.backgroundColor = 'green';
+    } else {
+      currSkill.style.backgroundColor = 'blue';
+    }
+    this.modalService.dismissAll();
   }
 
   // ng-bootstrap modal from https://ng-bootstrap.github.io/#/components/modal/examples
   open(content, event) {
-    let selectedSkill = this.skillInfo[event.target.id];
-    this.modalService.open(content, { ariaLabelledBy: 'skill-name' });
-    document.getElementById('footer-skill-points').innerText =
-      'Skill points available: ' + this.skillPoints.toString();
-    document.getElementById('skill-name').innerText = selectedSkill.name;
-    document.getElementById('skill-desc').innerText = selectedSkill.desc;
-    if (selectedSkill.cost === 1) {
-      document.getElementById('skill-cost').innerText =
-        selectedSkill.cost.toString() + ' skill point';
+    if (this.allSkills[event.target.id].purchased) {
+      this.modalService.dismissAll();
     } else {
-      document.getElementById('skill-cost').innerText =
-        selectedSkill.cost.toString() + ' skill points';
-    }
+      let selectedSkill = this.allSkills[event.target.id];
+      this.skillId = event.target.id;
+      this.modalService.open(content, { ariaLabelledBy: 'skill-name' });
+      document.getElementById('footer-skill-points').innerText =
+        'Skill points available: ' + this.skillPoints.toString();
+      document.getElementById('skill-name').innerText = selectedSkill.name;
+      document.getElementById('skill-desc').innerText = selectedSkill.desc;
+      if (selectedSkill.cost === 1) {
+        document.getElementById('skill-cost').innerText =
+          selectedSkill.cost.toString() + ' skill point';
+      } else {
+        document.getElementById('skill-cost').innerText =
+          selectedSkill.cost.toString() + ' skill points';
+      }
 
-    if (this.skillPoints >= selectedSkill.cost) {
-      this.canPurchase = true;
-    } else {
-      this.canPurchase = false;
+      if (this.skillPoints >= selectedSkill.cost) {
+        this.canPurchase = true;
+        document.getElementById('footer-skill-points').style.color = 'black';
+      } else {
+        this.canPurchase = false;
+        document.getElementById('footer-skill-points').style.color = 'red';
+      }
     }
   }
 }
